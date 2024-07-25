a8fb1950c25a85faac5d92ba1f79098dfca87958
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-25 09:15:37 +0100

    update thing model schema

--- flask/app/models.py
+++ flask/app/models.py
@@ -8,9 +8,10 @@ from app import db
 class Thing(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    number_of_things: so.Mapped[int] = so.mapped_column(sa.Integer)
-    timestamp: so.Mapped[datetime] = so.mapped_column(
+    quantity: so.Mapped[int] = so.mapped_column(sa.Integer, index=True)
+    created_at: so.Mapped[datetime] = so.mapped_column(
         sa.DateTime,
         index=True,
         default=lambda: datetime.now(timezone.utc),
     )
+    updated_at: so.Mapped[datetime] = so.mapped_column(sa.DateTime, index=True, nullable=True)
