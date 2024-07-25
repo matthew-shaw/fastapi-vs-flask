de9a33ed9d51624f80444f5e51e40a540c8985c1
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-25 14:30:33 +0100

    add colours enum

--- flask/app/models.py
+++ flask/app/models.py
@@ -1,13 +1,26 @@
 from datetime import datetime, timezone
+from enum import StrEnum, auto
 
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
 
+class Colours(StrEnum):
+    RED = auto()
+    GREEN = auto()
+    BLUE = auto()
+    YELLOW = auto()
+    ORANGE = auto()
+    PURPLE = auto()
+    BLACK = auto()
+    WHITE = auto()
+
+
 class Thing(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    colour: so.Mapped[str] = so.mapped_column(sa.Enum(Colours), index=True)
     quantity: so.Mapped[int] = so.mapped_column(sa.Integer, index=True)
     created_at: so.Mapped[datetime] = so.mapped_column(
         sa.DateTime,
--- flask/migrations/versions/0d599512acf0_add_thing.py
+++ flask/migrations/versions/0d599512acf0_add_thing.py
@@ -1,8 +1,8 @@
 """add thing
 
-Revision ID: ff8bafcbff9e
+Revision ID: 0d599512acf0
 Revises:
-Create Date: 2024-07-25 09:14:43.910516
+Create Date: 2024-07-25 14:25:48.708940
 
 """
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "ff8bafcbff9e"
+revision = "0d599512acf0"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,12 +22,32 @@ def upgrade():
         "thing",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("name", sa.String(length=64), nullable=False),
+        sa.Column(
+            "colour",
+            sa.Enum(
+                "RED",
+                "GREEN",
+                "BLUE",
+                "YELLOW",
+                "ORANGE",
+                "PURPLE",
+                "BLACK",
+                "WHITE",
+                name="colours",
+            ),
+            nullable=False,
+        ),
         sa.Column("quantity", sa.Integer(), nullable=False),
         sa.Column("created_at", sa.DateTime(), nullable=False),
         sa.Column("updated_at", sa.DateTime(), nullable=True),
         sa.PrimaryKeyConstraint("id"),
     )
     with op.batch_alter_table("thing", schema=None) as batch_op:
+        batch_op.create_index(
+            batch_op.f("ix_thing_colour"),
+            ["colour"],
+            unique=False,
+        )
         batch_op.create_index(
             batch_op.f("ix_thing_created_at"),
             ["created_at"],
@@ -59,6 +79,7 @@ def downgrade():
         batch_op.drop_index(batch_op.f("ix_thing_quantity"))
         batch_op.drop_index(batch_op.f("ix_thing_name"))
         batch_op.drop_index(batch_op.f("ix_thing_created_at"))
+        batch_op.drop_index(batch_op.f("ix_thing_colour"))
 
     op.drop_table("thing")
     # ### end Alembic commands ###
