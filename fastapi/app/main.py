c09f2b8b31e5f5ecb8e1a9694b8eeed9de553e12
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-31 13:49:25 +0100

    use model

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -2,18 +2,24 @@ from pydantic import BaseModel
 
 from fastapi import FastAPI
 
-app = FastAPI()
-
 
 class Item(BaseModel):
     name: str
+    description: str | None = None
     price: float
-    is_offer: bool | None = None
+    tax: float | None = None
 
 
-@app.get("/")
-async def root():
-    return {"Hello": "World"}
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/items/{item_id}")
@@ -25,4 +31,4 @@ async def read_item(item_id: int, q: str | None = None):
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_id": item_id, **item.dict()}
