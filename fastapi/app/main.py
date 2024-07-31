3db92a6cd963b4e59d4b8efc188311267dfac430
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-07-31 12:06:14 +0100

    async functions

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -12,15 +12,15 @@ class Item(BaseModel):
 
 
 @app.get("/")
-def read_root():
+async def root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
+async def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
