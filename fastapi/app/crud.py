54b05c914167b8f91eda56a898e3fb09300fd164
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2024-08-12 15:27:25 +0100

    add placeholder methods

--- fastapi/app/crud.py
+++ fastapi/app/crud.py
@@ -3,10 +3,16 @@ from sqlalchemy.orm import Session
 
 
 def get_item(db: Session, item_id: int) -> models.Item | None:
+    """
+    Get an item.
+    """
     return db.query(models.Item).filter(models.Item.id == item_id).first()
 
 
-def get_items(db: Session, skip: int = 0, limit: int = 100) -> list[models.Item]:
+def list_items(db: Session, skip: int = 0, limit: int = 100) -> list[models.Item]:
+    """
+    List items.
+    """
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 
@@ -14,8 +20,25 @@ def create_item(
     db: Session,
     item: schemas.ItemCreate,
 ) -> models.Item:
+    """
+    Create an item.
+    """
     db_item = models.Item(**item.dict())
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def update_item(db: Session, item_id: int) -> models.Item | None:
+    """
+    Update an item.
+    """
+    return None
+
+
+def delete_item(db: Session, item_id: int) -> None:
+    """
+    Delete an item.
+    """
+    return None
--- fastapi/app/routers/items.py
+++ fastapi/app/routers/items.py
@@ -13,19 +13,44 @@ router = APIRouter(
 
 @router.post("/", response_model=schemas.Item)
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    """
+    Create an item.
+    """
     return crud.create_item(db, item=item)
 
 
 @router.get("/", response_model=list[schemas.Item])
-def read_items(
+def list_items(
     skip: int = 0,
     limit: int = 100,
     db: Session = Depends(get_db),
 ):
-    items = crud.get_items(db, skip=skip, limit=limit)
+    """
+    List items.
+    """
+    items = crud.list_items(db, skip=skip, limit=limit)
     return items
 
 
 @router.get("/{item_id}", response_model=schemas.Item)
 def read_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    Get an item.
+    """
     return crud.get_item(db, item_id=item_id)
+
+
+@router.put("/{item_id}", response_model=schemas.Item)
+def update_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    Update an item.
+    """
+    return crud.update_item(db, item_id=item_id)
+
+
+@router.delete("/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    Delete an item.
+    """
+    return crud.delete_item(db, item_id=item_id)
